a243aa33c4ba348cab8dad475b3008f7c1ca8d69
niloy32 <tanzimulhaque@ymail.com>
2021-03-04 05:34:00 +0600

    ggg

--- src/index.js
+++ src/index.js
@@ -3,21 +3,6 @@ import ReactDOM from "react-dom";
 import App from "./App";
 import "./App.css";
 import reportWebVitals from "./reportWebVitals";
-import RecipeReviewCard from "./icons";
-import Card from "@material-ui/core/Card";
-import CardHeader from "@material-ui/core/CardHeader";
-import CardMedia from "@material-ui/core/CardMedia";
-import CardContent from "@material-ui/core/CardContent";
-import CardActions from "@material-ui/core/CardActions";
-import Avatar from "@material-ui/core/Avatar";
-import IconButton from "@material-ui/core/IconButton";
-import Typography from "@material-ui/core/Typography";
-import { red } from "@material-ui/core/colors";
-import ShareIcon from "@material-ui/icons/Share";
-import ExpandMoreIcon from "@material-ui/icons/ExpandMore";
-import MoreVertIcon from "@material-ui/icons/MoreVert";
-import { makeStyles } from "@material-ui/core/styles";
-import TestCard from "./Test_icon";
 import "./index.css";
 <link
   href="https://maxcdn.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css"
